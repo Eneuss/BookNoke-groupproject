8d55f2dde21bf6b8a001c7365bef737204c54a86
Admin <Admin@192.168.1.101>
2025-01-11 16:06:00 +0000

    version 1.5

--- src/main/java/com/mycompany/booknook/BookNook.java
+++ src/main/java/com/mycompany/booknook/BookNook.java
@@ -27,42 +27,27 @@ public class BookNook {
         // initial menu
         while (true) {
             System.out.println("\n--- Menu ---");
-            System.out.println("1. Register");
-            System.out.println("2. Login");
-            System.out.println("3. Exit");
+            System.out.println("1. Group member A");
+            System.out.println("2. Group member B");
+            System.out.println("3. Group member C");
+            System.out.println("4. Exit");
             System.out.print("Select an option: ");
             int choice = scanner.nextInt();
             scanner.nextLine();
 
             switch (choice) {
                 case 1: // register
-                    adminController.addNewUser();
+                    menuCenter.groupMemberA(scanner);
                     break;
                 case 2:
-                    /* Part E of group member B Enea Rina that implements the
-                        login functionality to the program, making a distinguish
-                        between regular users and admins
-                    */ 
-                    System.out.print("Enter Username: ");
-                    String username = scanner.nextLine();
-                    System.out.print("Enter Password: ");
-                    String password = scanner.nextLine();
-                    boolean loginSuccess = login.login(username, password);
-                
-                    if (loginSuccess) {
-                        // Check if the logged-in user is an Admin
-                        if (login.getLoggedInUser() instanceof Admin) {
-                            System.out.println("Admin privileges granted.");
-                            menuCenter.AdminMenu(scanner, (Admin) login.getLoggedInUser());
-                        } else {
-                            System.out.println("Regular user logged in.");
-                            menuCenter.UserMenu(scanner);
-                        }
-                    }
+                    menuCenter.groupMemberB(scanner);
                     break;
                     
                 case 3:
+                    menuCenter.groupMemberC(scanner);
                     return ;
+                case 4:
+                    return;
                 default:
                     System.out.println("Invalid choice. Please try again.");
             }
