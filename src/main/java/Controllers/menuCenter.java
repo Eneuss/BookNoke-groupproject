51437c8cc30e385352735a802d45398d60e05077
Admin <Admin@192.168.1.101>
2025-01-11 16:08:39 +0000

    version 1.5

--- src/main/java/Controllers/menuCenter.java
+++ src/main/java/Controllers/menuCenter.java
@@ -5,37 +5,62 @@
 package Controllers;
 import java.util.Scanner;
 import Entities.Admin;
-import Controllers.productController;
+
 /**
  *
  * @author Admin
  */
 public class menuCenter {
     
+    // Part of Valerio Gerardi 
+    public static void groupMemberA(Scanner scanner) {
+        while(true) {
+            System.out.println("\nGroup member A 'Gerardi Valerio' functionalities:");
+            System.out.println("1. Search Book");
+            System.out.println("2. Buy product");
+            System.out.println("3. View order history");
+            System.out.print("4. Return to Group members menu ");
+            System.out.print("\nSelect an option: ");
+            
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break; // you can leave this as it is to exit this menu
+            }
+        }
+    }
     
-    public static void AdminMenu(Scanner scanner, Admin admin) {
-        while (true) {
-            System.out.println("\nAdmin Menu:");
+    
+    // Part of Enea Rina
+    public static void groupMemberB(Scanner scanner) {
+        while(true) {
+            System.out.println("\nGroup member B 'Rina Enea' functionalities:");
             System.out.println("1. Add a new item or category");
-            System.out.println("2. View All Regular Users");
-            System.out.println("3. Change an user details");
-            System.out.println("4. Delete user");
-            System.out.println("5. Edit Product list or categorys"); //Luis part
-            System.out.println("6. Delete Product"); //Luis part
-            System.out.println("7. Logout");
-            System.out.print("Select an option: ");
-
+            System.out.println("2. Log in as admin or regular user");
+            System.out.println("3. List of all users");
+            System.out.println("4. Change details of an active user in the system");
+            System.out.print("5. Return to Group members menu ");
+            System.out.print("\nSelect an option: ");
+            
             int choice = scanner.nextInt();
             scanner.nextLine();
 
             switch (choice) {
                 case 1:
-                    System.out.println("---Inventory Update!---");
+                    System.out.println("\n---Inventory Update!---");
                     System.out.println("What do you want to add:");
                     System.out.println("1. Add a new Book");
                     System.out.println("2. Add a new item accessory");
                     System.out.println("3. Add a new category");
-                    System.out.println("4. Return to menu");
+                    System.out.println("4. Return to previous menu");
                     System.out.print("Select an option: ");
                     int inventory = scanner.nextInt();
                     scanner.nextLine();
@@ -55,63 +80,85 @@ public class menuCenter {
                             System.out.println("Invalid selection!!!");
                             break;
                     }
-                    break;
                 case 2:
+                    System.out.println("\n---Login funtionalities---");
+                    System.out.println("1. Register");
+                    System.out.println("2. Login");
+                    System.out.println("3. Return to previous menu");
+                    System.out.print("Select an option: ");
+                    int choiceLogin = scanner.nextInt();
+                    scanner.nextLine();
+
+                    switch (choiceLogin) {
+                        case 1: // register
+                            adminController.addNewUser();
+                            break;
+                        case 2:
+                            System.out.print("Enter Username: ");
+                            String username = scanner.nextLine();
+                            System.out.print("Enter Password: ");
+                            String password = scanner.nextLine();
+                            boolean loginSuccess = login.login(username, password);
+
+                            if (loginSuccess) {
+                                // Check if the logged-in user is an Admin
+                                if (login.getLoggedInUser() instanceof Admin) {
+                                    System.out.println("Admin privileges granted.");
+                                    login.getLoggedInUser();
+                                } else {
+                                    System.out.println("Regular user logged in.");
+                                }
+                            }
+                            
+                            if (loginSuccess) {
+                                login.logout();
+                            }
+                            break;
                     
+                        case 3:
+                            break;
+                        default:
+                            System.out.println("Invalid choice. Please try again.");
+                    }
+                case 3:
                     adminController.viewAllUsers();
                     break;
-                case 3:
-                    adminController.editUser(scanner);
-                    break;
                 case 4:
-                    adminController.deleteUser(scanner);
+                    adminController.editUser(scanner);
                     break;
                 case 5:
                     break;
-                case 6:
-                    break;
-                case 7:
-                    login.logout();
-                    return;// Exit the admin section and returns to main menu
                 default:
                     System.out.println("Invalid choice, please try again.");
                     break;
             }
         }
-                
     }
     
     
-    
-    public static void UserMenu(Scanner scanner) {
-        while (true) {
-            System.out.println("\nUser Menu:");
-            System.out.println("1. Search book"); // Valerio part
-            System.out.println("2. Buy a Book or Accessory");
-            System.out.println("3. View order history");
-            System.out.println("4. Logout");
-            System.out.print("Select an option: ");
-
+    // Part of Luis 
+    public static void groupMemberC(Scanner scanner) {
+        while(true) {
+            System.out.println("\nGroup member A 'Gerardi Valerio' functionalities:");
+            System.out.println("1. List of products");
+            System.out.println("2. Signup account");
+            System.out.println("3. Modify products");
+            System.out.print("4. Return to Group members menu ");
+            System.out.print("\nSelect an option: ");
+            
             int choice = scanner.nextInt();
-            scanner.nextLine();  // Consume newline
-
+            scanner.nextLine();
+            
             switch (choice) {
                 case 1:
-                    //connect to the function that lets you search for a book
                     break;
                 case 2:
-                    //buy an item and also use the function of the case 1 to show the list again
+                    adminController.addNewUser();  // Enea and Luis share this function
                     break;
                 case 3:
-                    //function that displays the order history
                     break;
                 case 4:
-                    System.out.println("Logging out from user menu.");
-                    login.logout();
-                    return; // Exit the loop
-                default:
-                    System.out.println("Invalid choice, please try again.");
-                    break;
+                    break;  // you can leave this as it is to exit this menu
             }
         }
     }
